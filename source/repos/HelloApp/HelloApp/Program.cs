08451e7fea6b3345f7f0afa12032810bf4383a0d
Gudzichek <den.zubov.2005@mail.ru>
2025-07-15 08:32:22 +0300

    Aritmetics №2

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,3 +1,8 @@
-﻿Console.WriteLine("Введите значение x:");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(x *= x);
+﻿Console.WriteLine("Введите значение x1:");
+decimal x1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите значение x2:");
+decimal x2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите значение x3:");
+decimal x3 = Convert.ToDecimal(Console.ReadLine());
+decimal answer = x1 * x2 * x3;
+Console.WriteLine(answer);
