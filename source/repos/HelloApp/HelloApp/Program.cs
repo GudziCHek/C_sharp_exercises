30f56200e318f2521253c306f6c4faeb8d355dff
Gudzichek <den.zubov.2005@mail.ru>
2025-07-16 22:04:38 +0300

    Arithmetics №8

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,10 +1,20 @@
-﻿Console.WriteLine("Введите значение a:");
-decimal a = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Введите значение b:");
-decimal b = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Введите значение c:");
-decimal c = Convert.ToDecimal(Console.ReadLine());
-var result1 = (a + b) * c;
-var result2 = a * c + b * c;
-Console.WriteLine(result1);
-Console.WriteLine(result2);
+﻿Console.WriteLine("Введите диапозон чисел:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int x;
+if (x1 < x2)
+{
+    for (x = x1; x <= x2; x++)
+    {
+        int y = x * x - 2 * x + 1;
+        Console.Write("{0} ", y);
+    }
+}
+else
+{
+    for (x = x2; x <= x1; x++)
+    {
+        int y = x * x - 2 * x + 1;
+        Console.Write("{0} ", y);
+    }
+}
