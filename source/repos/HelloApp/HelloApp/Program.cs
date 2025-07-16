8e44555f24c0cd95f59ddadde8f403243afb4d37
Gudzichek <den.zubov.2005@mail.ru>
2025-07-16 22:33:43 +0300

    Data types A №2

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,8 +1,26 @@
-﻿Console.WriteLine("Введите радиус окружности:");
-decimal radius = Convert.ToDecimal(Console.ReadLine());
-
-decimal pi = (Decimal)Math.PI;
-decimal surface = 4 * pi * radius * radius;
-decimal volume = 4 / 3 * pi * radius * radius * radius;
-Console.WriteLine("Площадь окружности = {0:F2}", surface);
-Console.WriteLine("Объём окружности = {0}", volume);
+﻿Console.WriteLine("Введите числа:");
+decimal a = Convert.ToDecimal(Console.ReadLine());
+decimal b = Convert.ToDecimal(Console.ReadLine());
+try
+{
+    var c = a + b;
+    var d = a - b;
+    var e = a * b;
+    var f = a / b;
+    var g = a % b;
+    Console.WriteLine("{0} + {1} = {2}", a, b, c);
+    Console.WriteLine("{0} - {1} = {2}", a, b, d);
+    Console.WriteLine("{0} x {1} = {2}", a, b, e);
+    Console.WriteLine("{0} / {1} = {2}", a, b, f);
+    Console.WriteLine("{0} mod {1} = {2}", a, b, g);
+}
+catch(DivideByZeroException)
+{
+    var c = a + b;
+    var d = a - b;
+    var e = a * b;
+    Console.WriteLine("{0} + {1} = {2}", a, b, c);
+    Console.WriteLine("{0} - {1} = {2}", a, b, d);
+    Console.WriteLine("{0} x {1} = {2}", a, b, e);
+    Console.WriteLine("Операции на деление на ноль нельзя.");
+}
