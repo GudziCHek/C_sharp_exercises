64e5cce032513b0cfb4e8e58967e119ae2d080fc
Gudzichek <den.zubov.2005@mail.ru>
2025-07-17 20:10:29 +0300

    Data types A №3

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,26 +1,5 @@
-﻿Console.WriteLine("Введите числа:");
-decimal a = Convert.ToDecimal(Console.ReadLine());
-decimal b = Convert.ToDecimal(Console.ReadLine());
-try
-{
-    var c = a + b;
-    var d = a - b;
-    var e = a * b;
-    var f = a / b;
-    var g = a % b;
-    Console.WriteLine("{0} + {1} = {2}", a, b, c);
-    Console.WriteLine("{0} - {1} = {2}", a, b, d);
-    Console.WriteLine("{0} x {1} = {2}", a, b, e);
-    Console.WriteLine("{0} / {1} = {2}", a, b, f);
-    Console.WriteLine("{0} mod {1} = {2}", a, b, g);
-}
-catch(DivideByZeroException)
-{
-    var c = a + b;
-    var d = a - b;
-    var e = a * b;
-    Console.WriteLine("{0} + {1} = {2}", a, b, c);
-    Console.WriteLine("{0} - {1} = {2}", a, b, d);
-    Console.WriteLine("{0} x {1} = {2}", a, b, e);
-    Console.WriteLine("Операции на деление на ноль нельзя.");
-}
+﻿Console.WriteLine("Введите три буквы:");
+char a = Convert.ToChar(Console.ReadLine());
+char b = Convert.ToChar(Console.ReadLine());
+char c = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("{0} {1} {2}", c, b, a);
