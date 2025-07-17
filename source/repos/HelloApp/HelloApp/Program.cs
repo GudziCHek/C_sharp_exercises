2bf97456e5a91e0afc09de4ef3e994ab738eb07d
Gudzichek <den.zubov.2005@mail.ru>
2025-07-17 20:45:41 +0300

    Data types A №7

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,22 +1,24 @@
-﻿Console.WriteLine("Введите расстояние в метрах:");
-decimal distance = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Введите время в часах:");
-decimal time_per_hour = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Введите время в минутах:");
-decimal time_per_minute = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Введите время в секундах:");
-decimal time_per_second = Convert.ToDecimal(Console.ReadLine());
+﻿Console.WriteLine("Введите число:");
+decimal number = Convert.ToDecimal(Console.ReadLine());
 
-decimal total_sec = time_per_hour * 3600 + time_per_minute * 60 + time_per_second;
-if (total_sec == 0)
-{
-    Console.WriteLine("Ошибка: время не может быть нулевым.");
-    return;
-}
+decimal multiplication1 = number * 1;
+decimal multiplication2 = number * 2;
+decimal multiplication3 = number * 3;
+decimal multiplication4 = number * 4;
+decimal multiplication5 = number * 5;
+decimal multiplication6 = number * 6;
+decimal multiplication7 = number * 7;
+decimal multiplication8 = number * 8;
+decimal multiplication9 = number * 9;
+decimal multiplication10 = number * 10;
 
-decimal speed_in_meters_per_sec = distance / total_sec;
-decimal speed_in_kilometers_per_hour = (distance / 1000) / (total_sec / 3600);
-decimal speed_in_miles_per_hour = (distance / 1609) / (total_sec / 3600);
-Console.WriteLine("Скорость в м/c = {0}", speed_in_meters_per_sec);
-Console.WriteLine("Скорость в км/ч = {0}", speed_in_kilometers_per_hour);
-Console.WriteLine("Скорость в миль/ч = {0}", speed_in_miles_per_hour);
+Console.WriteLine("{0} x 1 = {1}", number, multiplication1);
+Console.WriteLine("{0} x 2 = {1}", number, multiplication2);
+Console.WriteLine("{0} x 3 = {1}", number, multiplication3);
+Console.WriteLine("{0} x 4 = {1}", number, multiplication4);
+Console.WriteLine("{0} x 5 = {1}", number, multiplication5);
+Console.WriteLine("{0} x 6 = {1}", number, multiplication6);
+Console.WriteLine("{0} x 7 = {1}", number, multiplication7);
+Console.WriteLine("{0} x 8 = {1}", number, multiplication8);
+Console.WriteLine("{0} x 9 = {1}", number, multiplication9);
+Console.WriteLine("{0} x 10 = {1}", number, multiplication10);
