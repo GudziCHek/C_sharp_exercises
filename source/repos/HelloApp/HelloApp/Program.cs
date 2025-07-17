d48f573e65cd4cf5d465dc7f60ed9cad4ee09216
Gudzichek <den.zubov.2005@mail.ru>
2025-07-17 20:40:40 +0300

    Data types A №6

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,21 +1,22 @@
-﻿Console.WriteLine("Введите длину прямоугольника:");
-decimal width = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Введите ширину прямоугольника:");
-decimal height = Convert.ToDecimal(Console.ReadLine());
-if (width != 0 && height != 0)
-{    
-    decimal Perimeter = (width + height) * 2;
-    decimal Area = width * height;
-    decimal Diagonal = (decimal)Math.Sqrt((double)(width * width + height * height));
-    Console.WriteLine("Периметр = {0}", Perimeter);
-    Console.WriteLine("Площадь = {0}", Area);
-    Console.WriteLine("Диагональ = {0}", Diagonal);
-}
-else
+﻿Console.WriteLine("Введите расстояние в метрах:");
+decimal distance = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите время в часах:");
+decimal time_per_hour = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите время в минутах:");
+decimal time_per_minute = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите время в секундах:");
+decimal time_per_second = Convert.ToDecimal(Console.ReadLine());
+
+decimal total_sec = time_per_hour * 3600 + time_per_minute * 60 + time_per_second;
+if (total_sec == 0)
 {
-    decimal Perimeter = (width + height) * 2;
-    decimal Area = width * height;
-    Console.WriteLine("Периметр = {0}", Perimeter);
-    Console.WriteLine("Площадь = {0}", Area);
-    Console.WriteLine("Диагональ невозможно вычислить.");
+    Console.WriteLine("Ошибка: время не может быть нулевым.");
+    return;
 }
+
+decimal speed_in_meters_per_sec = distance / total_sec;
+decimal speed_in_kilometers_per_hour = (distance / 1000) / (total_sec / 3600);
+decimal speed_in_miles_per_hour = (distance / 1609) / (total_sec / 3600);
+Console.WriteLine("Скорость в м/c = {0}", speed_in_meters_per_sec);
+Console.WriteLine("Скорость в км/ч = {0}", speed_in_kilometers_per_hour);
+Console.WriteLine("Скорость в миль/ч = {0}", speed_in_miles_per_hour);
