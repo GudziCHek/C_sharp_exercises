2600fb876c775fb7b241d2bc9df5c7c7da217b04
Gudzichek <den.zubov.2005@mail.ru>
2025-07-19 08:35:48 +0300

    Data types A №8

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,24 +1,8 @@
-﻿Console.WriteLine("Введите число:");
-decimal number = Convert.ToDecimal(Console.ReadLine());
+﻿Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
 
-decimal multiplication1 = number * 1;
-decimal multiplication2 = number * 2;
-decimal multiplication3 = number * 3;
-decimal multiplication4 = number * 4;
-decimal multiplication5 = number * 5;
-decimal multiplication6 = number * 6;
-decimal multiplication7 = number * 7;
-decimal multiplication8 = number * 8;
-decimal multiplication9 = number * 9;
-decimal multiplication10 = number * 10;
-
-Console.WriteLine("{0} x 1 = {1}", number, multiplication1);
-Console.WriteLine("{0} x 2 = {1}", number, multiplication2);
-Console.WriteLine("{0} x 3 = {1}", number, multiplication3);
-Console.WriteLine("{0} x 4 = {1}", number, multiplication4);
-Console.WriteLine("{0} x 5 = {1}", number, multiplication5);
-Console.WriteLine("{0} x 6 = {1}", number, multiplication6);
-Console.WriteLine("{0} x 7 = {1}", number, multiplication7);
-Console.WriteLine("{0} x 8 = {1}", number, multiplication8);
-Console.WriteLine("{0} x 9 = {1}", number, multiplication9);
-Console.WriteLine("{0} x 10 = {1}", number, multiplication10);
+Random random = new Random();
+int rndNum = random.Next(num1, num2 + 1);
+Console.WriteLine(rndNum);
