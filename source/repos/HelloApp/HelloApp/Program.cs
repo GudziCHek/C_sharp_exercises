696b9271cecf15bdadc37686e599e0c7760ba235
Gudzichek <den.zubov.2005@mail.ru>
2025-07-21 21:59:32 +0300

    Array №2

--- source/repos/HelloApp/HelloApp/Program.cs
+++ source/repos/HelloApp/HelloApp/Program.cs
@@ -1,10 +1,43 @@
-﻿int[] numbers = new int[5];
+﻿Console.WriteLine("Введите длину массива:");
+int x = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i < 5; i++)
+int[] array = new int[x];
+for (int i = 0; i < array.Length; i++)
 {
-    Console.WriteLine($"Введите число {i + 1}:");
-    numbers[i] = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Значение {i + 1} - ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
 }
 
-Array.Reverse(numbers);
-Console.WriteLine(string.Join(" ", numbers));
+string input;
+
+do
+{
+    Console.WriteLine("Введите число для поиска (или 'end' для выхода):");
+    input = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(input) || input.ToLower() == "end")
+    {
+        break;
+    }
+
+    if (int.TryParse(input, out int y))
+    {
+        bool found = false;
+
+        foreach (int num in array)
+        {
+            if (num == y)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        Console.WriteLine(found ? $"Число {y} существует" : $"Число {y} не существует");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка! Введите число или 'end'.");
+    }
+}
+while (true);
